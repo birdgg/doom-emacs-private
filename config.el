4e6ced9b5ba403c3aa033996133eb9c8ba710bfe
birdgg <birdgg@birdggs-MBP.local>
2018-08-27 18:11:36 +0800

    add org capture extension

--- config.el
+++ config.el
@@ -1,4 +1,5 @@
 ;;;  -*- lexical-binding: t; -*-
+
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
                          ("org"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
@@ -13,6 +14,8 @@
  js-indent-level 2
  org-bullets-bullet-list '("·")
  )
+
+(add-hook 'org-load-hook '(lambda () (setq org-modules (append '(org-protocol org-habit) org-modules))))
 
 (defun my-company-transformer (candidates)
   (let ((completion-ignore-case t))
@@ -36,10 +39,20 @@
         org-log-done 'time
         org-fontify-done-headline nil
         org-ellipsis " ↴ ")
-
-  (setq org-capture-templates
-    '(("t" "Todo" entry
-     (file+headline "inbox.org" "Inbox")
-     "* TODO %?\nCREATED: %u\n%i" :prepend t :kill-buffer t))))
+  (setq org-tag-alist '(("@office" . ?o)
+                         ("@home" . ?h)
+                         (:newline)
+                         ("hifi" . ?c)))
+  (setq org-todo-keywords
+        '((sequence "[ ](t)" "[-](p)" "[?](m)" "|" "[X](d)")
+          (sequence "TODO(T)" "|" "DONE(D)")
+          (sequence "NEXT(n)" "ACTIVE(a)" "WAITING(w)" "LATER(l)" "|" "CANCELLED(c)")))
+  (setq org-capture-templates `(
+    ("t" "Todo" entry (file+headline "inbox.org" "Inbox")
+     "* TODO %?\nCREATED: %u\n%i" :prepend t :kill-buffer t)
+	("l" "Protocol Link" entry (file+headline "inbox.org" "Inbox")
+        "* [[%:link][%:description]] \nCREATED: %u" :prepend t)
+   ))
+   )
 
 (load! "+bindings")
